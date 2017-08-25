d97e64426d3fb8aacb6eaebae2c2e2e998380b58
jonnytownend <jonathan.townend@gmail.com>
2017-08-25 16:06:12 +0100

    Changed file structure for clarity

--- Dependancies/lineIntersection.js
+++ Dependancies/lineIntersection.js
@@ -1,16 +1,16 @@
 function checkLineIntersection(ray, block) {
     // if the lines intersect, the result contains the x and y of the intersection (treating the lines as infinite) and booleans for whether line segment 1 or line segment 2 contain the point
-	
+
 	var line1StartX = ray.origin.x;
 	var line1StartY = ray.origin.y;
 	var line1EndX = ray.origin.x + ray.vector.x;
 	var line1EndY = ray.origin.y + ray.vector.y;
-	
+
 	var line2StartX = block.origin.x;
 	var line2StartY = block.origin.y;
 	var line2EndX = line2StartX + block.vector.x;
 	var line2EndY = line2StartY + block.vector.y;
-    
+
     var denominator, a, b, numerator1, numerator2;
     var x = 0;
     var y = 0;
@@ -38,14 +38,14 @@ function checkLineIntersection(ray, block) {
     if (b > 0 && b < 1) {
         onLine2 = true;
     }
-    
+
     if (onLine1 == true && onLine2 == true) {
         // if we cast these lines infinitely in both directions, they intersect here:
         x = line1StartX + (a * (line1EndX - line1StartX));
         y = line1StartY + (a * (line1EndY - line1StartY));
 		return new Vector2(x, y);
     }
-    
+
     /*
 	else
 		return false;
--- Dependancies/vector2.js
+++ Dependancies/vector2.js
@@ -1,60 +1,56 @@
-function Vector2(x,y) { //Simple vector3 class
-    this.x = x;
-    this.y = y;
+class Vector2 {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
     
-    this.len = function() {
-        sqr = (this.x*this.x) + (this.y*this.y);
+    len() {
+        var sqr = (this.x*this.x) + (this.y*this.y);
         return Math.sqrt( sqr );
     }
     
-    this.add = function( vect ) {
+    add( vect ) {
         var x = this.x + vect.x;
         var y = this.y + vect.y;
         
         return new Vector2(x,y);
     }
     
-    this.subtract = function( vect ) {
+    subtract( vect ) {
         var x = this.x - vect.x;
         var y = this.y - vect.y;
         
         return new Vector2(x,y);
     }
-	
-	this.scalarMult = function( scalar ) {
-		var x = this.x*scalar;
+    
+    scalarMult( scalar ) {
+        var x = this.x*scalar;
 		var y = this.y*scalar;
 		
 		return new Vector2(x,y);
-	}
-	
-	this.scale = function( scale ) {
-		var x = this.x*scale/this.len();
+    }
+    
+    scale( scale ) {
+        var x = this.x*scale/this.len();
 		var y = this.y*scale/this.len();
 		this.x = x;
 		this.y = y;
-	}
+    }
     
-    this.rotate = function( angle ) {
-        var x;
-        var y;
-		
+    rotate( angle ) {
+        var x, y;
 		x = this.x*Math.cos(angle) - this.y*Math.sin(angle);
 		y = this.x*Math.sin(angle) + this.y*Math.cos(angle);
 		this.x = x;
 		this.y = y;
     }
-	
-	this.dot = function( vect ) {
-		return this.x*vect.x + this.y*vect.y;
-	}
     
-    this.cross = function( vect ) {
-        
+    dot( vect ) {
+        return this.x*vect.x + this.y*vect.y;
     }
-	
-	this.angleBetween = function( vect ) {
-		var dot = this.dot( vect );
+    
+    angleBetween( vect ) {
+        var dot = this.dot( vect );
 		var mult = this.len() * vect.len();
 		if (mult==0) {
 			return false;
@@ -63,49 +59,41 @@ function Vector2(x,y) { //Simple vector3 class
 			var costheta = dot/mult;
 			return Math.acos( costheta );
 		}
-	}
-    
-    this.angleBetween2 = function( vect ) {
-        var angle = Math.atan2(this.y, this.x) - Math.atan2(vect.y, vect.x);
-        if (angle < 0)
-            angle += 2*Math.PI;
-        return angle;
     }
     
-    this.angleBetween3 = function( vect ) {
-        var v1 = this.copy();
-        v1.scale(1);
-        var v2 = vect.copy();
-        v2.scale(1);
-        return Math.acos(v1.dot(v2));
+    normal() {
+        return new Vector2(this.y, -this.x);
     }
-	
-	this.normal = function() {
-		return new Vector2(this.y, -this.x);
-	}
-	
-	this.set = function(x,y) {
-		this.x = x;
+    
+    set(x,y) {
+        this.x = x;
 		this.y = y;
-	}
+    }
     
-    this.copy = function() {
+    copy() {
         return new Vector2(this.x, this.y);
     }
     
-    this.angleToX = function() {
+    angleToX() {
         if (Math.sign(this.y) == Math.sign)
             return Math.atan2(this.y, this.x);
         else
             return Math.atan2(this.y, this.x);
     }
-	
-	this.drawFrom = function( context, x, y ) { //For html5 canvas use
+    
+    greateThan(vect) {
+        if (this.len() > vect.len())
+            return true;
+        else
+            return false;
+    }
+    
+    drawFrom(context, x, y) {
+        //For html5 canvas use
 		ctx.beginPath();
 		ctx.moveTo( x, y );
 		ctx.lineTo( x + this.x, y + this.y );
 		ctx.stroke();
 		ctx.closePath();
-	}
-
+    }
 }
